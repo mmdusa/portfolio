e04ac80c16c19bbbc5c51f56f91acf8adf09b01a
ARIA <108695355+mmdusa@users.noreply.github.com>
2025-12-08 13:35:46 +0100

    Revise Italian translations in translations.js
    Updated translations for recommendations and achievements section.

--- src/utils/translations.js
+++ src/utils/translations.js
@@ -326,14 +326,14 @@ export const translations = {
     // =========================
     recommendationsTitle: "Cover Letters",
     read_more: "Read more",
-    recommendations: [
+     recommendations: [
       {
         id: "quer",
         name: "Stefano Scanzio",
-        title: "Professor, Politecnico di Torino",
+        title: "Professore, Politecnico di Torino",
         avatar: "/certs/prof-quer.jpg",
         preview:
-          "It is my pleasure to write this recommendation letter for Mohammadhossein Rajabisohrani.",
+          "Mohammad è stato mio studente da settembre 2023 a gennaio 2024. Ha seguito un ottimo percorso di apprendimento...",
         content: [
           "It is my pleasure to write this recommendation letter for Mohammadhossein Rajabisohrani.I had the opportunity to get to know Mohammadhossein during the 2024-2025 academic year when he was pursuing his Bachelor's degree in Computer Science Engineering at Politecnico di Torino.",
           " I appreciated his attitude toward study during my course about Operating Systems in 2025. Mohammadhossein has shown a very deep interest in the subject, and he passed the exam with a mark of 30, placing his in the top 1-2% of the class.",
@@ -345,6 +345,7 @@ export const translations = {
       },
     ],
   },
+};
 
   it: {
     // =========================
